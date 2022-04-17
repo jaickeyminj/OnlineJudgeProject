9e6ffb53246934dc770b70b14f58195d171a1f2e
Jaickey Joy Minj <jaickey09@gmail.com>
2022-04-17 23:23:12 +0530

    Code changes in sign up page and form

--- OnlineJudge/signup/views.py
+++ OnlineJudge/signup/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
-
+import mysql.connector as sql
+# from .models import Post
+fn=''
+ln=''
+s=''
+email=''
+pwd=''
+un=''
 # Create your views here.
 def signaction(request):
-    return None
+    global fn,ln,s,email,pwd,un
+    if request.method=="POST":
+        #open connection to db
+        m=sql.connect(host="localhost",user="root",password="root",database='onlinejudge')
+        cursor=m.cursor
+        data=request.POST
+        #fetch the values
+        for key,value in data.items():
+            if key=="first_name":
+                fn=value
+            if key=="last_name":
+                ln=value
+            if key=="sex":
+                s=value
+            if key=="email":
+                email=value
+            if key=="password":
+                pwd=value
+            if key=="user_name":
+                un=value
+        #store data in db online judge
+        c="insert into users('{}','{}','{}','{}','{}','{}')".format(fn,ln,s,email,pwd,un)
+        cursor.execute(c)
+        m.commit()
+    
+    return render(request,'signup_page.html')
