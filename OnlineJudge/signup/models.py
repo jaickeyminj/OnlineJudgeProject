b63c66c741ca69f1584aa25bbde4fc74f761f1a2
Jaickey Joy Minj <jaickey09@gmail.com>
2022-04-20 21:31:28 +0530

    making model class for Problems

--- OnlineJudge/signup/models.py
+++ OnlineJudge/signup/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    problem_id = models.CharField(max_length=20)
+    problem_title= models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
