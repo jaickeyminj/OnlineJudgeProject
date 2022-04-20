2c5679ab7de439e1574999f313c2aebee278d701
Jaickey Joy Minj <jaickey09@gmail.com>
2022-04-20 21:51:59 +0530

    addition of other models

--- OnlineJudge/signup/models.py
+++ OnlineJudge/signup/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Problem(models.Model):
-    problem_id = models.CharField(max_length=20)
+    problem_id = models.CharField(max_length=20,primary_key=True)
     problem_title= models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
+    difficulty = models.CharField(max_length=10,default='Easy')
+
+class Solution(models.Model):
+    code = models.CharField(max_length=3000)
+    problem_id = models.OneToOneField(Problem, verbose_name=('problem_id'),primary_key=True,on_delete=models.CASCADE)
+    verdict = models.CharField(max_length=20)
+    time = models.DateField(default=timezone.now)
+
+class TestCase(models.Model):
+    input = models.CharField(max_length=200)
+    output = models.CharField(max_length=200)
+    problem_id = models.OneToOneField(Problem, verbose_name=('problem_id'),primary_key=True,on_delete=models.CASCADE)
