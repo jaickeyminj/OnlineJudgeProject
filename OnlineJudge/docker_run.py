27e5dc8267abcf7628e9eb3b9aca5d13e75ab7da
Jaickey Joy Minj <jaickey09@gmail.com>
2022-05-07 23:00:55 +0530

    success run of docker test file

--- OnlineJudge/docker_run.py
+++ OnlineJudge/docker_run.py
@@ -1,6 +1,9 @@
 import os,subprocess
 
-subprocess.run('docker build -t python:0.1 D:/DataSet', shell=True)
-output = subprocess.run('docker run python:0.1 › output.txt', shell=True, capture_output=True)
+subprocess.run('docker build -t python:0.1 C:/Users/jaick/Documents/OnlineJudgeProject/OnlineJudgeProject/OnlineJudge', shell=True)
+output = subprocess.run('docker run python:0.1 > output1.txt', shell=True, capture_output=True)
 print(output.stderr)
 print(output.stdout)
+with open('output1.txt', 'r') as file:
+    content = file.read()
+    print(content)
