22da54c4d639f60a77d04efc8f8b0878e9923be1
Jaickey Joy Minj <jaickey09@gmail.com>
2022-05-08 11:32:31 +0530

    testing python user i/p with docker

--- OnlineJudge/HelloWorld.py
+++ OnlineJudge/HelloWorld.py
@@ -1,1 +1,2 @@
 print("hellomagic")
+print(input("enter name"))
--- OnlineJudge/docker_run.py
+++ OnlineJudge/docker_run.py
@@ -1,9 +1,10 @@
 import os,subprocess
 
-subprocess.run('docker build -t python:0.1 C:/Users/jaick/Documents/OnlineJudgeProject/OnlineJudgeProject/OnlineJudge', shell=True)
-output = subprocess.run('docker run python:0.1 > output1.txt', shell=True, capture_output=True)
-print(output.stderr)
-print(output.stdout)
+subprocess.run('docker build -t python:0.1 .', shell=True)
+output = subprocess.run('docker run -t -i python:0.1 > output1.txt', shell=True, capture_output=True)
+print(output.stderr.decode('UTF-8'))
+print(output.stdout.decode(('UTF-8')))
+print(output)
 with open('output1.txt', 'r') as file:
     content = file.read()
     print(content)
