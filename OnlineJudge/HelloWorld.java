2c52f0ffc4a1286f4b7081cb65cfd831a30c1bb0
Jaickey Joy Minj <jaickey09@gmail.com>
2022-05-12 22:56:06 +0530

    Testing docker with java files

--- OnlineJudge/HelloWorld.java
+++ OnlineJudge/HelloWorld.java
@@ -1,18 +1,35 @@
-import java.util.Scanner;
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        
-        int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
-        //System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        //System.out.println();
-        num2 = sc.nextInt();
-        
-        sc.close();
-        sum = num1 * num2;
-        System.out.println(""+sum);
-    }
+import java.util.Scanner;
+
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+
+        
+
+        int num1, num2, sum;
+
+        // Scanner sc = new Scanner(System.in);
+
+        // //System.out.println("Enter First Number: ");
+
+        // num1 = sc.nextInt();
+
+        
+
+        // //System.out.println();
+
+        // num2 = sc.nextInt();
+
+        
+
+        // sc.close();
+
+        // sum = num1 * num2;
+
+        System.out.println("HelloWorld");
+
+    }
+
 }
--- OnlineJudge/docker_run.py
+++ OnlineJudge/docker_run.py
@@ -1,12 +1,22 @@
 import os,subprocess
 
-subprocess.run('docker build -t python:0.1 .', shell=True) 
-file = open('data.txt','r').read()
-output = subprocess.run('docker run -i python:0.1 > output1.txt', shell=True, capture_output=True,input=file.encode())
+# subprocess.run('docker build -t python:0.1 .', shell=True) 
+# file = open('data.txt','r').read()
+# output = subprocess.run('docker run -i python:0.1 > output1.txt', shell=True, capture_output=True,input=file.encode())
+# # output = subprocess.run('docker run -it -v /tmp/python/:/python/data python:0.1 data.txt data/output2.txt', shell=True, capture_output=True)
+# print(output.stderr.decode('UTF-8'))
+# print(output.stdout.decode(('UTF-8')))
+# print(output)
+# with open('output1.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+subprocess.run('docker build -t java:0.1 .', shell=True) 
+# file = open('data.txt','r').read()
+output = subprocess.run('docker run -i java:0.1 > outputJAVA.txt', shell=True, capture_output=True)
 # output = subprocess.run('docker run -it -v /tmp/python/:/python/data python:0.1 data.txt data/output2.txt', shell=True, capture_output=True)
 print(output.stderr.decode('UTF-8'))
 print(output.stdout.decode(('UTF-8')))
 print(output)
-with open('output1.txt', 'r') as file:
-    content = file.read()
-    print(content)
+# with open('output1.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
